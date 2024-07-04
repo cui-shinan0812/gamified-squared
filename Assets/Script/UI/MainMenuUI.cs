c8a3c5181a5f0454b3ac5eff003febfccd04c543
LeungKC0102 <leungkc0102@gmail.com>
2024-07-04 12:12:57 +0800

    UI implementing

---
+++ Assets/Script/UI/MainMenuUI.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Runtime.InteropServices; // Ensure this using directive is at the top
+
+
+public class MainMenuUI : MonoBehaviour
+{
+    [SerializeField] private Button GameLaunchButton;
+    [SerializeField] private Button GameSettingsButton;
+    [SerializeField] private Button ReturnButton;
+    [SerializeField] private GameObject GameLaunchUI;
+    [SerializeField] private GameObject GameSettingsUI;
+    
+    // [SerializeField] private Button quitButton;
+    
+    // Assuming "myDynamicLibrary.dll" exports a function named "send_controllight"
+    // that takes two parameters: a string (for IP) and an int (for port).
+    [DllImport("myDynamicLibrary", CallingConvention = CallingConvention.Cdecl)]
+    private static extern void send_controllight([MarshalAs(UnmanagedType.LPWStr)] string targetIP, int targetPort);    
+    string targetIP = "169.254.161.94";
+    int targetPort = 4628;
+    
+    private void Awake() {
+        GameSettingsUI.SetActive(false);
+        GameLaunchButton.onClick.AddListener(() => {
+            send_controllight(targetIP, targetPort);
+            Debug.Log("send_controllight(" + targetIP + ", " + targetPort + ")");
+        });
+        
+        GameSettingsButton.onClick.AddListener(() =>
+        {
+            GameLaunchUI.SetActive(false);
+            GameSettingsUI.SetActive(true);
+            
+        });
+        
+        ReturnButton.onClick.AddListener(() =>
+        {
+            GameSettingsUI.SetActive(false);
+            GameLaunchUI.SetActive(true);
+        });
+        
+        // Click quitButton
+        // quitButton.onClick.AddListener(Application.Quit);
+        
+        // Time.timeScale = 1f;
+    }
+}
