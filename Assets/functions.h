41e014bca0060c51cd7b6fbd5c7a63f0303f89a5
LeungKC0102 <leungkc0102@gmail.com>
2024-07-04 12:17:11 +0800

    functions.h changed to LibCaller.h

--- Assets/functions.h
+++
@@ -1,9 +0,0 @@
-#ifndef FUNCTONS_H
-#define FUNCTONS_H
-#include <string>
-
-extern "C" unsigned char Check_sum(unsigned char* r_d, unsigned short Len);
-extern "C" unsigned char find_crc(unsigned char out_crc);
-
-extern "C" void send_controllight(wchar_t* targetIP, int targetPort);
-#endif
